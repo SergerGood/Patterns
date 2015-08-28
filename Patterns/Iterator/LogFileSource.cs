a370cf0bca2f688b2e29199d5eeef70948fa9107
SergerGood <serzher@inbox.ru>
2015-08-28 13:24:00 +0300

    Iterator

---
+++ Patterns/Iterator/LogFileSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Patterns.Iterator
+{
+    public class LogFileSource : IEnumerable<LogEntry>
+    {
+        private readonly string logFileName;
+
+
+        public LogFileSource(string logFileName)
+        {
+            this.logFileName = logFileName;
+        }
+
+
+        public IEnumerator<LogEntry> GetEnumerator()
+        {
+            foreach (var line in File.ReadAllLines(logFileName))
+            {
+                yield return LogEntry.Parse(line);
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Patterns/LogEntry.cs
+++ Patterns/LogEntry.cs
@@ -1,7 +1,22 @@
-﻿namespace Patterns
+﻿using System;
+
+
+namespace Patterns
 {
     public class LogEntry
     {
-         
+        private string line;
+
+
+        private LogEntry(string line)
+        {
+            this.line = line;
+        }
+
+
+        public static LogEntry Parse(string line)
+        {
+            return new LogEntry(line);
+        }
     }
 }
