84ea0c8caea1c0ea5dfd3b94751d8f7bb7185347
SergerGood <serzher@inbox.ru>
2015-08-28 14:50:31 +0300

    Visitor

---
+++ Patterns/Visitor/AbstractVisitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Patterns.Visitor
+{
+    public class AbstractVisitor
+    {
+        public abstract class LogEntryVisitorBase : InterfaceVisitor.ILogEntryVisitor
+        {
+            public virtual void Visit(InterfaceVisitor.ExceptionLogEntry exceptionLogEntry)
+            {
+            }
+
+
+            public virtual void Visit(InterfaceVisitor.LogEntry logEntry)
+            {
+            }
+        }
+
+
+        public class DatabaseExceptionLogEntrySaver
+        {
+            public void SaveLogEntry(InterfaceVisitor.LogEntry logEntry)
+            {
+                logEntry.Accept(new ExceptionLogEntryVisitor(this));
+            }
+
+
+            private class ExceptionLogEntryVisitor : LogEntryVisitorBase
+            {
+                private readonly DatabaseExceptionLogEntrySaver parent;
+
+
+                public ExceptionLogEntryVisitor(DatabaseExceptionLogEntrySaver parent)
+                {
+                    this.parent = parent;
+                }
+
+
+                public override void Visit(InterfaceVisitor.ExceptionLogEntry exceptionLogEntry)
+                {
+                    //parent.SaveException(exceptionLogEntry);
+                }
+            }
+        }
+    }
+}
---
+++ Patterns/Visitor/InterfaceVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Patterns.Visitor
+{
+    public class InterfaceVisitor
+    {
+        public interface ILogEntryVisitor
+        {
+            void Visit(ExceptionLogEntry exceptionLogEntry);
+            void Visit(LogEntry logEntry);
+        }
+
+
+        public abstract class LogEntry
+        {
+            public abstract void Accept(ILogEntryVisitor logEntryVisitor);
+        }
+
+
+        public class ExceptionLogEntry : LogEntry
+        {
+            public override void Accept(ILogEntryVisitor logEntryVisitor)
+            {
+                logEntryVisitor.Visit(this);
+            }
+        }
+    }
+}
