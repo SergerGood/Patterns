41523c457bb33baccd3aa79ce2c116d84e40d6bd
SergerGood <serzher@inbox.ru>
2015-08-28 14:59:37 +0300

    AbstractFactory

---
+++ Patterns/AbstractFactory/LogSaver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+
+namespace Patterns.AbstractFactory
+{
+    public class LogSaver
+    {
+        private readonly DbProviderFactory factory;
+
+
+        public LogSaver(DbProviderFactory factory)
+        {
+            this.factory = factory;
+        }
+
+
+        public void Save(IEnumerable<LogEntry> logEntries)
+        {
+            using (var connection = factory.CreateConnection())
+            {
+                SetConnectionString(connection);
+                using (var command = factory.CreateCommand())
+                {
+                    SetCommandArguments(logEntries);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void SetConnectionString(DbConnection connection)
+        {
+        }
+
+
+        private void SetCommandArguments(IEnumerable<LogEntry> logEntry)
+        {
+        }
+    }
+}
